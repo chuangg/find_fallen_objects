130f41967a1ea9695699a4ceeddbb4ccc50b9623
abhi1092 <abhi1092@gmail.com>
2023-02-28 20:37:04 +0000

    submission running

--- run_interface.sh
+++ run_interface.sh
@@ -1,1 +1,2 @@
-conda run -n fallen_obj_challenge_env --no-capture-output python interface.py --display=:4
+conda run -n fallen_obj_challenge_env --no-capture-output python docker/interface.py --display=:4
+conda run -n fallen_obj_challenge_env --no-capture-output python docker/evaluate_submission.py
---
+++ run_submission.sh
@@ -0,0 +1,1 @@
+conda run -n fallen_obj_challenge_env --no-capture-output python docker/test.py
