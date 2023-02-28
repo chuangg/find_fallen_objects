eb8833aac78965d15e900bbd1d91429c5a0bd9c2
abhi1092 <abhi1092@gmail.com>
2023-02-28 21:51:54 +0000

    moving interface file

---
+++ docker/run_interface.sh
@@ -0,0 +1,2 @@
+conda run -n fallen_obj_challenge_env --no-capture-output python interface.py --display=:4
+conda run -n fallen_obj_challenge_env --no-capture-output python evaluate_submission.py
--- run_interface.sh
+++
@@ -1,2 +0,0 @@
-conda run -n fallen_obj_challenge_env --no-capture-output python docker/interface.py --display=:4
-conda run -n fallen_obj_challenge_env --no-capture-output python docker/evaluate_submission.py
