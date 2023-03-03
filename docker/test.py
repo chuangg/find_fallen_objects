bfe6e5b40189a02a1d0256f6babd0d8e98c8c68f
abhi1092 <abhi1092@gmail.com>
2023-03-03 15:11:53 -0500

    adding hostname and porter parameters

--- docker/test.py
+++ docker/test.py
@@ -2,6 +2,7 @@ import socket
 import pickle
 import random
 import io
+import argparse
 
 def send(soc: socket.socket, data: bytes):
     soc.sendall(bytes(f"{len(data):<{15}}", 'utf-8'))
@@ -14,17 +15,23 @@ def receive(soc: socket.socket):
         buffer.write(soc.recv(4096))
     return pickle.loads(buffer.getvalue())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-    soc.connect(("localhost", 2590))
-    send(soc, pickle.dumps({"op": "reset"}))
-    obs, info = receive(soc)
-    send(soc, pickle.dumps({"op": "observation_space"}))
-    observation_space = receive(soc)
-    send(soc, pickle.dumps({"op": "action_space"}))
-    action_space = receive(soc)
-    for i in range(10):
-       
-        send(soc, pickle.dumps({"op": "step", "action": random.randint(1, 6)}))
-        result = receive(soc)
-        
-        obs, reward, done, info = result
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hostname', type=str, default="localhost", help='Hostname to connect the interface')
+    parser.add_argument('--port', type=str, default=2590, help='Port number to connect the interface')
+    args = parser.parse_args()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        soc.connect((args.hostname, args.port))
+        send(soc, pickle.dumps({"op": "reset"}))
+        obs, info = receive(soc)
+        send(soc, pickle.dumps({"op": "observation_space"}))
+        observation_space = receive(soc)
+        send(soc, pickle.dumps({"op": "action_space"}))
+        action_space = receive(soc)
+        for i in range(10):
+
+            send(soc, pickle.dumps({"op": "step", "action": random.randint(1, 6)}))
+            result = receive(soc)
+
+            obs, reward, done, info = result
