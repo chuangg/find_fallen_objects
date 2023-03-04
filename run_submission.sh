44577a15fa7e850ec7ad421959b863758bdc8095
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-03-03 20:30:36 -0500

    Update run_submission.sh

--- run_submission.sh
+++ run_submission.sh
@@ -1,1 +1,1 @@
-conda run -n fallen_obj_challenge_env --no-capture-output python docker/test.py --hostname $1 --port $2
+conda run -n fallen_obj_challenge_env --no-capture-output python docker/test.py --hostname $1 --port $2 --steps $3
